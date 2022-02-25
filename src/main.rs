d84e5a8fb56b61a8d70936a3de0e95a3b99a4694
Katerina N. Shananina <eshananina@rambler-co.ru>
2022-02-25 12:15:56 +0300

    Draw Enter and Exit red

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,33 @@ use generator::DungeonBuilder;
 use narrative::longpath::LongPathNarrativeBuilder;
 use narrative::NarrativeBuilder;
 
+const WALLS_ID: i16 = 1;
+const ENTER_ID: i16 = 2;
+const WALLS_COLOR: i16 = pancurses::COLOR_GREEN;
+const ENTER_COLOR: i16 = pancurses::COLOR_RED;
+
 fn draw_dungeon(dungeon: &Dungeon) {
     let window = pancurses::initscr();
     pancurses::curs_set(0);
+    pancurses::start_color();
+    pancurses::init_pair(WALLS_ID, WALLS_COLOR, pancurses::COLOR_BLACK);
+    pancurses::init_pair(ENTER_ID, ENTER_COLOR, pancurses::COLOR_BLACK);
+    window.color_set(WALLS_ID);
     for y in 0..dungeon.size_y {
         for x in 0..dungeon.size_x {
             match dungeon.map[x][y] {
                 Field::Wall => window.mvprintw(y as i32, x as i32, "#"),
                 Field::Floor => window.mvprintw(y as i32, x as i32, "."),
-                Field::Exit => window.mvprintw(y as i32, x as i32, ">"),
-                Field::Enter => window.mvprintw(y as i32, x as i32, "<"),
+                Field::Exit => {
+                    window.color_set(ENTER_ID);
+                    window.mvprintw(y as i32, x as i32, ">");
+                    window.color_set(WALLS_ID)
+                },
+                Field::Enter => {
+                    window.color_set(ENTER_ID);
+                    window.mvprintw(y as i32, x as i32, "<");
+                    window.color_set(WALLS_ID)
+                },
                 _ => window.mvprintw(y as i32, x as i32, "/"),
             };
         }
@@ -27,7 +44,7 @@ fn draw_dungeon(dungeon: &Dungeon) {
     pancurses::endwin();
 }
 
-fn main() {
+fn main() {    
     let dfs_gen = DFSGenerator {
         size_x: 16,
         size_y: 16,
@@ -37,6 +54,7 @@ fn main() {
     let narrative_builder = LongPathNarrativeBuilder {};
     narrative_builder.fill(&mut dungeon);
     draw_dungeon(&dungeon);
+
     let cellular_gen = CellularAutomataGenerator {
         size_x: 50,
         size_y: 50,
@@ -48,6 +66,7 @@ fn main() {
     let narrative_builder = LongPathNarrativeBuilder {};
     narrative_builder.fill(&mut dungeon);
     draw_dungeon(&dungeon);
+
     let bsp_gen = BSPGenerator {
         size_x: 50,
         size_y: 50,
